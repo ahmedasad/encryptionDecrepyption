a454e8b6f7546cc724c20f4795f09c553273b8a1
Syed Ahmed <ahmedasad2010@gmail.com>
2020-08-11 16:01:07 +0500

    isempty edittext

--- app/src/main/java/com/uhfsolutions/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/uhfsolutions/roomdatabase/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.uhfsolutions.roomdatabase
 
 import android.content.Intent
-import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
 import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import com.uhfsolutions.roomdatabase.bundle.TransformBundle
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlin.coroutines.CoroutineContext
 
@@ -33,6 +34,28 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
     }
 
+    private fun isEmpty(view: ViewGroup):EmptyResult{
+        var result = EmptyResult(false,"")
+        for(item in view.children){
+            if(item is ViewGroup)
+                isEmpty(item)
+            else if(item is EditText){
+                if(isEmptyOrNull(item.text.toString())){
+                    // setting values
+                    result.empty = true
+                    result.name = if(item.tag!=null) item.tag.toString() else "Check if any field left empty"
+                    return result
+                }
+            }
+        }
+        return result
+    }
+
+    fun isEmptyOrNull(text: String):Boolean{
+        return text == null || text.equals("")
+    }
+
+
     override val coroutineContext: CoroutineContext
         get() = IO
 }
@@ -43,3 +66,5 @@ class Car{
     var model:String? = null
     var quantity:Int = 1
 }
+
+internal class EmptyResult(var empty: Boolean, var name: String)
